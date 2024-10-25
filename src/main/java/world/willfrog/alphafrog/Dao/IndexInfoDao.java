288006893930146f84f3e199466e19b76570cc50
Frog Weng <froggywch@gmail.com>
2024-10-25 10:31:08 +0800

    Save Work

--- src/main/java/world/willfrog/alphafrog/Dao/IndexInfoDao.java
+++ src/main/java/world/willfrog/alphafrog/Dao/IndexInfoDao.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import world.willfrog.alphafrog.Entity.IndexInfo;
 
+import java.util.List;
+
 @Mapper
 public interface IndexInfoDao {
 
@@ -11,6 +13,16 @@ public interface IndexInfoDao {
           "VALUES (#{tsCode}, #{name}, #{fullName}, #{market}, #{publisher}, #{indexType}, #{category}, #{baseDate}, #{basePoint}, #{listDate}, #{weightRule}, #{desc}, #{expDate})" +
           "ON CONFLICT (ts_code) DO NOTHING")
   int insertIndexInfo(IndexInfo indexInfo);
+
+  @Insert("<script>" +
+          "INSERT INTO alphafrog_index_info (ts_code, name, fullname, market, publisher, index_type, category, base_date, base_point, list_date, weight_rule,\"desc\", exp_date) " +
+          "VALUES " +
+          "<foreach collection='indexInfoList' item='indexInfo' separator=','>" +
+          "(#{indexInfo.tsCode}, #{indexInfo.name}, #{indexInfo.fullName}, #{indexInfo.market}, #{indexInfo.publisher}, #{indexInfo.indexType}, #{indexInfo.category}, #{indexInfo.baseDate}, #{indexInfo.basePoint}, #{indexInfo.listDate}, #{indexInfo.weightRule}, #{indexInfo.desc}, #{indexInfo.expDate})" +
+          "</foreach>" +
+          "ON CONFLICT (ts_code) DO NOTHING" +
+          "</script>")
+  int insertIndexInfos(List<IndexInfo> indexInfoList);
 
 
 }
